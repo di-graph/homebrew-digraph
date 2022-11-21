dbacaafb52558841d2d7eb0e77b635c6a4baf502
Bahar Shah <bahar@getdigraph.com>
2022-11-21 16:52:05 -0500

    add formula

---
+++ Formula/digraph.rb
@@ -0,0 +1,27 @@
+class Digraph < Formula
+  desc "Monitor and detect policy violations in infrastructure deploys using Digraph"
+  homepage "https://www.getdigraph.com"
+  url "https://github.com/di-graph/digraph-cli/archive/refs/tags/v0.0.12.tar.gz"
+  sha256 "01d23f9478f2b75350d879d85c307e34fdb50a06348d7f6011f5c570d3b143b4"
+  license "MIT"
+  head "https://github.com/di-graph/digraph-cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    bin_path = buildpath/"src/github.com/di-graph/digraph"
+    bin_path.install Dir["*"]
+    cd bin_path do
+      # Install the compiled binary into Homebrew's `bin` - a pre-existing
+      # global variable
+      system "go", "build", "-o", bin/"digraph", "."
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/digraph validate 2>&1", 1)
+    assert_match "Error: must specify api-key as argument or set it within a .env file", output
+  end
+end
